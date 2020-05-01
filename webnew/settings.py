68331cf92f065aa8bf20098189748695b263bc70
hp <oluwaboriife@gmail.com>
2020-05-01 11:36:00 +0100

    Update ALLOWED_HOSTS with site and development server URL

--- webnew/settings.py
+++ webnew/settings.py
@@ -31,7 +31,7 @@ import os
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 #DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['bori-lair.herokuapp.com', '127.0.0.1:8000','127.0.0.1']
