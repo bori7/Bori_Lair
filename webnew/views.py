af0e1d10963a253389e1742b2574075bbcf0ce74
hp <oluwaboriife@gmail.com>
2020-05-01 05:06:06 +0100

    Update ALLOWED_HOSTS with site and development server URL

--- webnew/views.py
+++ webnew/views.py
@@ -5,7 +5,7 @@ import operator
 import random
 from pymongo import MongoClient
 
-Myclient = MongoClient('mongodb://oluwabori:moses2490@ds331548.mlab.com:31548/heroku_z0sxdn7q',connectTimeoutMS=30000,
+Myclient = MongoClient('mongodb://oluwabori:moses2490@ds331548.mlab.com:31548/heroku_z0sxdn7q/?retryWrites=true',connectTimeoutMS=30000,
                      socketTimeoutMS=None,
                      socketKeepAlive=True)
 mydb = Myclient.get_default_database()
